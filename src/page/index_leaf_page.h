6d3ba878a2c0d1620d4b24bd40edef5f0bef3942
Xinlong Chen <xl_chen@zju.edu.cn>
2022-02-04 20:00:06 +0800

    add index leaf page define

---
+++ src/page/index_leaf_page.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "page/fixed_page.h"
+
+template<typename T>
+class index_leaf_page : public fixed_page {
+public:
+    using fixed_page<T>::fixed_page;
+    void init(int field_size) {
+        fixed_page<T>::init(fixed_page);
+        this->magic_ref() = PAGE_INDEX_LEAF;
+    }
+
+    std::pair<int, index_leaf_page> split(int cur_id) {
+        auto pw = fixed_page<T>::split(cur_id);
+        return { pw.first, 
+            *reinterpret_cast<index_leaf_page*>(&pw.second)
+        };
+    }
+
+};
